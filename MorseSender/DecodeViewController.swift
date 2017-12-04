7e0d75b13c3faed0481cb5d5e3a1186b487c3ee1
Krasios <krasios@github.com>
2017-12-03 23:41:25 -0800

    added a way to keep track of intervals

--- MorseSender/DecodeViewController.swift
+++ MorseSender/DecodeViewController.swift
@@ -12,29 +12,55 @@ class DecodeViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var decodeBtn: UIButton!
     @IBOutlet weak var redBtn: UIButton!
+    var lastAct : Date?
+    var actDur = [Double]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let lpgr : UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
-        lpgr.minimumPressDuration = 0.5
-        lpgr.delegate = self
-        lpgr.delaysTouchesBegan = true
-        self.redBtn.addGestureRecognizer(lpgr)
+        let presser : UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+        presser.minimumPressDuration = 0.5
+        presser.delegate = self
+        presser.delaysTouchesBegan = true
+        self.redBtn.addGestureRecognizer(presser)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func longTap(_ sender: UILongPressGestureRecognizer) {
+    
+    @IBAction func decode(_ sender: UIButton) {
+        lastAct = nil
+        print(actDur)
+    }
+    @objc func longTap(_ sender: UILongPressGestureRecognizer) {
         print("Long tap")
         if sender.state == .ended {
-            print("UIGestureRecognizerStateEnded")
-            //Do Whatever You want on End of Gesture
+            print("PressEnded")
+            let last = lastAct
+            lastAct = Date()
+            let interval = lastAct!.timeIntervalSince(last!)
+            if interval < 1.5{
+                actDur.append(1)
+            }else{
+                actDur.append(3)
+            }
         }
         else if sender.state == .began {
-            print("UIGestureRecognizerStateBegan.")
-            //Do Whatever You want on Began of Gesture
+            print("PressBegan.")
+            if let last = lastAct {
+                lastAct = Date()
+                let interval = lastAct?.timeIntervalSince(last)
+                if interval! < 1.5{
+                    actDur.append(1)
+                }else{
+                    actDur.append(3)
+                }
+            }else{//begins new message
+                actDur.removeAll()
+                lastAct = Date()
+            }
+            
         }
     }
         
